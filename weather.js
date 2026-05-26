2c148c93fe5d3e429dbba8a43e9339c117f4d770
adrtitan88 <37352006+adrtitan88@users.noreply.github.com>
2026-05-26 13:23:25 -0500

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,23 @@
+const apiKey = "YOUR_API_KEY";
+
+fetch(`https://api.tomorrow.io/v4/weather/realtime?location=30.2752,-98.8719&apikey=${apiKey}`)
+.then(res => res.json())
+.then(data => {
+
+  const values = data.data.values;
+
+  document.getElementById("temp").innerText =
+    Math.round(values.temperature) + "°";
+
+  document.getElementById("rain").innerText =
+    Math.round(values.precipitationProbability) + "%";
+
+  document.getElementById("humidity").innerText =
+    Math.round(values.humidity) + "%";
+
+  document.getElementById("wind").innerText =
+    Math.round(values.windSpeed) + " mph";
+
+  document.getElementById("condition").innerText =
+    "Current Weather";
+});
